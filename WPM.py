8017dbc3423c6270e98d116887ed21d4ae0c2e11
Ammar Alaee <63842198+I3mk132@users.noreply.github.com>
2024-02-18 00:46:05 +0300

    version 1.2 (#2)

--- WPM.py
+++ WPM.py
@@ -2,6 +2,30 @@ import curses
 from curses import wrapper
 
 
+def second(stdscr):
+
+    curses.init_pair(
+        1,
+        curses.COLOR_GREEN,
+        curses.COLOR_MAGENTA
+    )
+    curses.init_pair(
+        2,
+        curses.COLOR_RED,
+        curses.COLOR_BLACK
+    )
+    curses.init_pair(
+        2,
+        curses.COLOR_WHITE,
+        curses.COLOR_BLACK
+    )
+    stdscr.clear()
+    stdscr.addstr(1, 0, 'Hello world!')
+    stdscr.refresh()
+    key = stdscr.getkey()
+    print(key)
+
+
 def main(stdscr):
     curses.init_pair(
         1,
@@ -26,3 +50,4 @@ def main(stdscr):
 
 
 wrapper(main)
+wrapper(second)
